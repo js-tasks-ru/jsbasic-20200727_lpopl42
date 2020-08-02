13021358260a1227375fde69a7289dbf17dceb15
Andrei Dubouski <lpopl42@gmail.com>
2020-08-02 21:00:48 +0300

    1 модуль, задачи 1 и 3

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,17 @@
  * @param {number} n
  * @returns {number}
  */
+"use strict"
 function factorial(n) {
-  // ваш код...
+
+  if (n==0){
+    return (1)
+  } else {
+    let res=n;
+    while (n>1) {
+      n--
+      res=res*n
+      }
+      return (res);
+  }
 }
--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,6 +3,14 @@
  * @param {string} str
  * @returns {string}
  */
+'use strict'
 function ucFirst(str) {
-  // ваш код...
+
+  if (str) {
+    let ret = str[0].toUpperCase();
+    let rezultStr = str.slice(1, str.length + 1);
+      return (`${ret}${rezultStr}`)
+  } else {
+      return ('')
+  }
 }
