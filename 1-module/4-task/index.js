dccf4a92956d2b49027a356d0aea2d6c2b054200
Andrei Dubouski <lpopl42@gmail.com>
2020-08-03 10:03:50 +0300

    1 блок, задачи 4,5

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,6 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  str=str.toLowerCase();
+  return (str.includes("xxx")||str.includes("1xbet"))
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,8 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length>maxlength){
+    str=`${str.substr(0, maxlength-1)}…`
+  }
+  return (str)
 }
